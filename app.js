08130e586ff60650a363fcbd7fe96767fa48f79d
Diego Porto <dporto@qualityalive.pt>
2020-07-30 02:25:16 +0100

    Update app.js

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ const figures = require('./routes/api/figures')
 app.use('/api/figures', figures)
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
